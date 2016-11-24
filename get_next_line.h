55e658ed4980be92d16f5de7aa6f2981eaa31d37
Robin MONNIER <rmonnier@e3r10p21.42.fr>
2016-11-24 17:39:45 +0100

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmonnier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/15 17:59:36 by rmonnier          #+#    #+#             */
+/*   Updated: 2016/11/24 13:28:49 by rmonnier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+# define BUFF_SIZE 32
+
+typedef	struct				s_struct
+{
+		int					fd;
+		char				*str;
+		char				*offset;
+		struct s_struct		*next;
+		struct s_struct		*prev;
+}							t_buffer;
+
+int 						get_next_line(const int fd, char **line);
+
+#endif
